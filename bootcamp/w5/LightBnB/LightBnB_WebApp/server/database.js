d5c7da75c069110131dea7cb578d6806ff66216f
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-03-10 16:56:53 -0500

    add updated getAllProperties function

--- bootcamp/w5/LightBnB/LightBnB_WebApp/server/database.js
+++ bootcamp/w5/LightBnB/LightBnB_WebApp/server/database.js
@@ -45,7 +45,6 @@ const getUserWithId = function(id) {
     FROM users WHERE 
     id = $1;`, [id])
     .then((result) => {
-      
       return result.rows[0];
     })
     .catch((err) => {
@@ -68,7 +67,7 @@ const addUser =  function(user) {
            RETURNING *;`
     , [user['name'],user['email'],user['password']])
     .then((result) => {
-      console.log(result)
+      console.log(result);
       return result.rows[0];
     })
     .catch((err) => {
@@ -88,7 +87,7 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
   return pool
-    .query(`SELECT reservations.* FROM reservations
+    .query(`SELECT reservations.*, properties.* FROM reservations
     JOIN properties ON reservations.property_id = properties.id
     JOIN property_reviews ON properties.id = property_reviews.property_id
     WHERE reservations.guest_id = $1
@@ -96,7 +95,8 @@ const getAllReservations = function(guest_id, limit = 10) {
     ORDER BY reservations.start_date
     LIMIT $2; `, [guest_id, limit])
     .then((result) => {
-      return result.rows[0];
+      console.log(result.rows)
+      return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
@@ -113,14 +113,55 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+  const queryParams = [];
+  
+  let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
+  `;
+  
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` AND owner_id = $${queryParams.length}`;
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += `AND cost_per_night > $${queryParams.length} `;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `AND cost_per_night < $${queryParams.length} `;
+  }
 
+  queryString += `GROUP BY properties.id `;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(property_reviews.rating) >=  $${queryParams.length} `;
+  }
+
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  
+  // console.log(queryString, queryParams);
+  
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
-      
       return result.rows;
     })
     .catch((err) => {
+     
       console.log(err.message);
     });
 };
@@ -133,9 +174,36 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(`INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms, active)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+          RETURNING *;`
+    , [
+      property.owner_id,
+      property.title,
+      property.description,
+      property.thumbnail_photo_url,
+      property.cover_photo_url,
+      property.cost_per_night,
+      property.street,
+      property.city,
+      property.province,
+      property.post_code,
+      property.country,
+      property.parking_spaces,
+      property.number_of_bathrooms,
+      property.number_of_bedrooms,
+      true,
+
+    ])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.addProperty = addProperty;
+
+//Lloyd Jefferson
+//asherpoole@gmx.com
