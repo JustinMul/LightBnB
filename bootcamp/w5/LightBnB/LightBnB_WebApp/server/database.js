9265157a12c503b120902c9ad7f6b592107a81a7
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-03-10 17:30:39 -0500

    add the create listing feature

--- bootcamp/w5/LightBnB/LightBnB_WebApp/server/database.js
+++ bootcamp/w5/LightBnB/LightBnB_WebApp/server/database.js
@@ -95,7 +95,7 @@ const getAllReservations = function(guest_id, limit = 10) {
     ORDER BY reservations.start_date
     LIMIT $2; `, [guest_id, limit])
     .then((result) => {
-      console.log(result.rows)
+    
       return result.rows;
     })
     .catch((err) => {
@@ -174,8 +174,9 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
+  console.log('PROPERTY', property);
   return pool
-    .query(`INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms, active)
+    .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms, active)
           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
           RETURNING *;`
     , [
@@ -194,9 +195,9 @@ const addProperty = function(property) {
       property.number_of_bathrooms,
       property.number_of_bedrooms,
       true,
-
     ])
     .then((result) => {
+      console.log(result.rows[0]);
       return result.rows[0];
     })
     .catch((err) => {
