e975b3f9ea87571b566c02e5c5b2e32c54a2c34a
Justin Mulroney <95982839+JustinMul@users.noreply.github.com>
2022-04-04 18:03:11 -0400

    Delete bootcamp/w1 directory

--- bootcamp/w1/d1-focal/sum.js
+++
@@ -1,4 +0,0 @@
-const args = process.argv.slice(2);
-console.log(Number(args[0]) + Number(args[1]));
-
-
--- bootcamp/w1/stretch/reverse.js
+++
@@ -1,8 +0,0 @@
-let reverseString = '';
-for (let i = 2; i < process.argv.length; i++) {
-  for (let j = process.argv[i].length - 1; j >= 0; j--) {
-    reverseString += process.argv[i][j];
-  }
-  console.log(reverseString);
-  reverseString = '';
-}
